8c329e3aaf040112cec7cce774c27c01a87a43b4
soup1997 <soup1997@naver.com>
2023-04-03 20:08:38 +0900

    2023-04-03

--- src/main.py
+++ src/main.py
@@ -7,21 +7,15 @@ from utils.utils import *
 from utils.icp import icp
 
 pose = [-81.4857, 9.0564, 0] # x, y, heading
+pose = [0, 0, 0] # x, y, heading
 prev_pcd = None
 
-<<<<<<< HEAD
 def callback(pcd):
     global prev_pcd, pose
     
     while pcd.data is None:
         rospy.loginfo('Lidar data is empty...')
         return None
-=======
-def lidar_callback(pcd):
-    while pcd.data is None:
-        rospy.loginfo('Lidar data is empty...')
-        return
->>>>>>> 7a1e94463fcdc0f23c77d9cb0dd911554c624ead
     
     current_local_pcd = pre_processing(pcd)
  
@@ -33,6 +27,7 @@ def lidar_callback(pcd):
     
     pose_T = v2t(pose)
     pose = t2v(np.dot(H2d, pose_T))
+    print(pose)
     draw_pose(pose)
     
     prev_pcd = current_local_pcd
@@ -40,6 +35,7 @@ def lidar_callback(pcd):
 if __name__ == '__main__':
     rospy.init_node('ICP', anonymous=True)
     rospy.loginfo('ICP' + ' is now working...')
-    rospy.Subscriber('/velodyne_points', PointCloud2, lidar_callback, queue_size=3)
+    rospy.Subscriber('/velodyne_points', PointCloud2, callback, queue_size=3)
     
-    rospy.spin()
+    while not rospy.is_shutdown():
+        rospy.spin()
